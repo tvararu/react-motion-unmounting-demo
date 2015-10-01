24a2390ccfa5c826fa8f2baad09359a808fd11a7
Theodor Vararu <theo@vararu.org>
2015-10-01 09:12:38 +0100

    Render a few boxes.

--- react/components/Test.jsx
+++ react/components/Test.jsx
@@ -1,7 +1,29 @@
-import { Component } from 'react'
+import { Component, PropTypes } from 'react'
+
+export class Box extends Component {
+  static displayName = 'Box'
+
+  static propTypes = {
+    children: PropTypes.string.isRequired
+  }
+
+  render () {
+    return <div { ...this.props }>{ this.props.children }</div>
+  }
+}
 
 export default class Test extends Component {
   static displayName = 'Test'
+
+  state = {
+    boxes: [
+      'Mary',
+      'Had',
+      'A',
+      'Little',
+      'Lamb'
+    ]
+  }
 
   constructor (props) {
     super(props)
@@ -9,6 +31,6 @@ export default class Test extends Component {
   }
 
   render () {
-    return <div>Hello world!</div>
+    return <div>{ this.state.boxes.map((val, idx) => <Box key={ idx }>{ val }</Box>) }</div>
   }
 }
